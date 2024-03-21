55a40b31ab92b4de9fd16d233fbdcb299fc416c6
EmmieIO <markonuoha6@gmail.com>
2024-03-21 01:18:19 +0100

    Our main is now ready to pull

--- api/core/settings.py
+++ api/core/settings.py
@@ -75,16 +75,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    
-     'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgres://propertypal_user:GpEg9kaDKVt4PLMsCRXzjGR0DYF8UgNO@dpg-cnt73uun7f5s73f7f6fg-a.oregon-postgres.render.com/propertypal',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
