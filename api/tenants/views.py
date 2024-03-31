4a4581995909d5e5016046be06da13793f3d74e6
ahmed-my <136060573+ahmed-my@users.noreply.github.com>
2024-03-31 12:06:12 +0100

    Update views.py. remove page render

--- api/tenants/views.py
+++ api/tenants/views.py
@@ -8,17 +8,3 @@ from .serializers import TenantSerializer
 class TenantViewSet(viewsets.ModelViewSet):
     queryset = Tenant.objects.all()
     serializer_class = TenantSerializer
-
-def api_documentation(request):
-    tenants = Tenant.objects.all()
-    context = {
-        'tenants': tenants,
-    }
-    return render(request, 'tenants/api.html', context)
-
-def tenant_list(request):
-    get_all_tenants = Tenant.objects.all()
-    context = {
-    	'display_tenants': get_all_tenants,
-    }
-    return render(request, 'tenants/tenant_list.html', context)
